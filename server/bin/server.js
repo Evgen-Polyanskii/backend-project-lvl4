45f04f6d88aa529cd84384f449991382c3f4974a
Evgen-Polyanskii <M4x1mOrlov@yandex.ru>
2021-12-11 13:53:56 +0300

    Настройка Heroku

--- server/bin/server.js
+++ server/bin/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 getApp().listen(port, '0.0.0.0', (err, address) => {
   if (err) {
-    fastify.log.error(err)
+    getApp.log.error(err)
     process.exit(1)
   }
   console.log(`Server is running on port: ${port}`);
